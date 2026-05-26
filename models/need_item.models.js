22620ee9283e5d9a13640c57e1b8bba289297f77
amorima <40240119@esmad.ipp.pt>
2026-05-26 15:05:17 +0100

    Refactor need item model to correct primary key assignment and improve field definitions

--- models/need_item.models.js
+++ models/need_item.models.js
@@ -1,12 +1,16 @@
 export default (sequelize, DataTypes) => sequelize.define("need item", {
-   id_pedido: {
+   id_item: {
      type: DataTypes.INTEGER,
      primaryKey: true,
+     autoIncrement: true,
+     allowNull: false,
+   },
+   id_pedido: {
+     type: DataTypes.INTEGER,
      allowNull: false,
    },
    tipo_bem_servico: {
      type: DataTypes.STRING(50),
-     primaryKey: true,
      allowNull: false,
    },
    publico: {
